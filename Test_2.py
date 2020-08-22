5d9e7945505a3878929b1e2a85d5ae38ab10270f
Jayas P Jacob <jayaspjacob@gmail.com>
2020-08-22 11:44:11 +0530

    UI

--- Test_2.py
+++ Test_2.py
@@ -1,29 +1,24 @@
 from tkinter import *
-import  pyttsx3
+import pyttsx3
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 import seaborn as sns
 import tensorflow.keras
-from tkinter import  filedialog
+from tkinter import filedialog
 from PIL import ImageTk, Image, ImageOps
-import  numpy as np
+import numpy as np
 
-
-engine = pyttsx3.init()
+# engine = pyttsx3.init()
 window = Tk()
-
-
 window.title('Covid Checker')
 
 
-
-def predict_ct():
-    model = tensorflow.keras.models.load_model('MODELS/keras_model.h5')
+def predict(file):  # predict function
+    model = tensorflow.keras.models.load_model('MODELS/keras_model.h5')  # loading the model
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    filename = filedialog.askopenfilename(title="Open Image")
-    test_image = Image.open(filename)
+    test_image = Image.open(file)
     image_show = ImageTk.PhotoImage(test_image)
     image_label = Label(window,image = image_show)
     image_label.image = image_show
@@ -35,72 +30,49 @@ def predict_ct():
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
     data[0] = normalized_image_array
     prediction = model.predict(data)
-    engine.say("hi")
     print(prediction)
     lael = Label(window,text = prediction).pack()
-   
+    return prediction
 
 
-def plot():
-   
-    model = tensorflow.keras.models.load_model('MODELS/keras_model.h5')
-
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-    filename = filedialog.askopenfilename(title="Open Image")
-    image = Image.open(filename)
-    np.set_printoptions(suppress=True)
-
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    image_array = np.asarray(image)
-
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    data[0] = normalized_image_array
-
-    prediction = model.predict(data)
-    print(prediction)
-
+def plot(prediction):
     f, ax = plt.subplots(figsize=(11, 9))
     x_axis_label = "+-"
-    sns.heatmap(prediction,Label="Covid",xticklabels=x_axis_label)
+    sns.heatmap(prediction, Label="Covid", xticklabels=x_axis_label)
 
     canvas = FigureCanvasTkAgg(f,
                                master=window)
     canvas.draw()
 
-    
     canvas.get_tk_widget().pack()
 
-   
     toolbar = NavigationToolbar2Tk(canvas,
                                    window)
     toolbar.update()
 
-
     canvas.get_tk_widget().pack()
-
-
+def run():
+    filename = filedialog.askopenfilename(title = "Open Image")
+    predict(filename)
+    plot(prediction)
 
 window.geometry("500x500")
-test_button = Button(master=window,command = predict_ct,height=2,
+test_button = Button(master=window,command = run,height=2,
                      width=10,
                      text="Test")
 
 
-plot_button = Button(master=window,
-                     command=plot,
-                     height=2,
-                     width=10,
-                     text="Plot")
+# plot_button = Button(master=window,
+#                      command=plot,
+#                      height=2,
+#                      width=10,
+#                      text="Plot")
 
 
 
 
 test_button.pack()
-plot_button.pack()
+# plot_button.pack()
 
 
 window.mainloop()
--- test_3.py
+++ test_3.py
@@ -1,22 +1,18 @@
 from tkinter import *
-import  pyttsx3
+import pyttsx3
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 import seaborn as sns
 import tensorflow.keras
-from tkinter import  filedialog
+from tkinter import filedialog
 from PIL import ImageTk, Image, ImageOps
-import  numpy as np
-
+import numpy as np
 
 engine = pyttsx3.init()
 window = Tk()
-
-
 window.title('Covid Checker')
-
 
 
 def predict_ct():
@@ -25,7 +21,7 @@ def predict_ct():
     filename = filedialog.askopenfilename(title="Open Image")
     test_image = Image.open(filename)
     image_show = ImageTk.PhotoImage(test_image)
-    image_label = Label(window,image = image_show)
+    image_label = Label(window, image=image_show)
     image_label.image = image_show
     image_label.pack()
     np.set_printoptions(suppress=True)
@@ -37,70 +33,30 @@ def predict_ct():
     prediction = model.predict(data)
     engine.say("hi")
     print(prediction)
-    lael = Label(window,text = prediction).pack()
-   
-
-
-def plot():
-   
-    model = tensorflow.keras.models.load_model('MODELS/keras_model.h5')
-
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-    filename = filedialog.askopenfilename(title="Open Image")
-    image = Image.open(filename)
-    np.set_printoptions(suppress=True)
-
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    image_array = np.asarray(image)
-
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    data[0] = normalized_image_array
-
-    prediction = model.predict(data)
-    print(prediction)
-
+    lael = Label(window, text=prediction).pack()
     f, ax = plt.subplots(figsize=(11, 9))
     x_axis_label = "+-"
-    sns.heatmap(prediction,Label="Covid",xticklabels=x_axis_label)
-
-    canvas = FigureCanvasTkAgg(f,
-                               master=window)
+    sns.heatmap(prediction, Label="Covid", xticklabels=x_axis_label)
+    canvas = FigureCanvasTkAgg(f,master=window)
     canvas.draw()
-
-    
     canvas.get_tk_widget().pack()
-
-   
-    toolbar = NavigationToolbar2Tk(canvas,
-                                   window)
+    toolbar = NavigationToolbar2Tk(canvas,window)
     toolbar.update()
-
-
     canvas.get_tk_widget().pack()
-
 
 
 window.geometry("500x500")
-test_button = Button(master=window,command = predict_ct,height=2,
+test_button = Button(master=window, command=predict_ct, height=2,
                      width=10,
-                     text="Test")
+                     text="Test",fg="black",bg='skyblue',font='Times 20 bold')
 
-
-plot_button = Button(master=window,
-                     command=plot,
+button_quit = Button(master=window,
+                     command=window.destroy,
                      height=2,
                      width=10,
-                     text="Plot")
+                     text="Quit",fg="black",bg='red',font='Times 20 bold')
 
-
-
-
-test_button.pack()
-plot_button.pack()
-
+test_button.pack(padx = 40,pady =30 , side = LEFT)
+button_quit.pack(padx = 50,pady = 40,side = LEFT)
 
 window.mainloop()
